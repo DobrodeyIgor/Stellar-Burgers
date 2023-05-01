720f66e21bfd76dc0587ce4638a258368b77bfa7
Dobrodey Igor <dobrodeiigor@yandex.ru>
2023-05-01 12:41:17 +0300

    routing and auth review

--- src/api/ingredients.service.ts
+++
@@ -1,8 +0,0 @@
-import { BASE_URL, checkResponse } from "./";
-const ingredientsRoute = "ingredients";
-
-export const fetchIngredients = () => {
-  return fetch(BASE_URL + ingredientsRoute, {
-    method: "GET",
-  }).then(checkResponse);
-};
